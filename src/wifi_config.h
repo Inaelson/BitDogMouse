64813ab627f2e60bf69dc868fd7650b3d3946bd7
Inaelson <inaelsonfranklin@gmail.com>
2025-02-18 22:37:44 -0300

    fixed wifi_config.h

--- src/wifi_config.h
+++ src/wifi_config.h
@@ -1,12 +1,12 @@
 #ifndef WIFI_CONFIG_H
 #define WIFI_CONFIG_H
 
-#define WIFI_SSID "Inaelson-2.4G"
-#define WIFI_PASS "21102000"
-#define SERVER_IP "192.168.18.2"
-#define UDP_PORT 5005
+#define WIFI_SSID "YOUR SSID HERE" // Substitua pelo nome da rede Wi-Fi
+#define WIFI_PASS "12345678" // Substitua pela senha da rede Wi-Fi
+#define SERVER_IP "YOUR IP HERE" // Substitua pelo IP do PC alvo
+#define UDP_PORT 5005 // Porta padrão do servidor UDP
 #define WOL_PORT 9  // Porta padrão do Wake-on-LAN
-#define TARGET_MAC {0x00, 0xE0, 0x70, 0xCF, 0x5B, 0x0B} // Substitua pelo MAC do PC alvo
+#define TARGET_MAC {0x00, 0x00, 0x00, 0x00, 0x00, 0x00} // Substitua pelo MAC do PC alvo
 
 #define MODE_MOUSE 0 // Modo mouse
 #define MODE_WOL 1 // Modo Wake-on-LAN
